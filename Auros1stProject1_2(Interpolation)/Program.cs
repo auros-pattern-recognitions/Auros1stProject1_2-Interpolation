14858d48793e48a638aa21c74c3740d63790707b
gyesung <sksmsdksl@naver.com>
2021-03-24 09:11:54 +0900

    Tridagonal matrix algorithms 해석

--- Auros1stProject1_2(Interpolation)/Program.cs
+++ Auros1stProject1_2(Interpolation)/Program.cs
@@ -204,102 +204,11 @@ namespace Interpolation
     #region 3차 곡선 보간법
     internal sealed class CubicSplineInterpolation
     {
-        //보간된 y값 리턴
-        public double? Interpolate(double p)
-        {
-            if (baseset)
-            {
-                double? result = 0;
-                int N = len - 1;
-
-                double[] h = X.Gap();  //x배열의 차이 저장
-                double[] D = Y.Gap().Scale(h, false);//스케일링
-                //반복되는 단일 값이 들어있는 시퀀스 생성
-                double[] s = Enumerable.Repeat(3.0, D.Length).ToArray();
-                //차이값 스케일링
-                double[] dD3 = D.Gap().Scale(s);
-                double[] a = Y;
-
-                // 3중 대각선 값들 정리
-                double[,] H = new double[N - 1, N - 1];
-                double[] diagVals = new double[N - 1];
-                for (int i = 1; i < N; i++)
-                {
-                    diagVals[i - 1] = 2 * (h[i - 1] + h[i]);
-                }
-
-                H = H.Diag(diagVals);
-
-                // 정사각 행렬이 아닐경우
-                if (H != null)
-                {
-                    for (int i = 0; i < N - 2; i++)
-                    {
-                        H[i, i + 1] = h[i + 1];
-                        H[i + 1, i] = h[i + 1];
-                    }
-
-                    double[] c = new double[N + 2];
-                    c = Enumerable.Repeat(0.0, N + 1).ToArray();
-
-                    // 3차 정방행렬 계산
-                    TridiagonalMatrix matrix = new TridiagonalMatrix();
-                    double[] solution = matrix.MaxtrixSolve(H, dD3);
-
-                    for (int i = 1; i < N; i++)
-                    {
-                        c[i] = solution[i - 1];
-                    }
-
-                    double[] b = new double[N];
-                    double[] d = new double[N];
-
-                    for (int i = 0; i < N; i++)
-                    {
-                        b[i] = D[i] - (h[i] * (c[i + 1] + 2.0 * c[i])) / 3.0;
-                        d[i] = (c[i + 1] - c[i]) / (3.0 * h[i]);
-                    }
-
-                    double Rx;
-
-                    try
-                    {
-                        Rx = X.First(m => m >= p);
-                    }
-                    catch   //p값이 x축 좌표계의 범위를 넘어갈때
-                    {
-                        return null;
-                    }
-
-                    //보간 하고자 하는 지점과 가장 가까운 x축 좌표를 찾는다.
-                    int iRx = lX.IndexOf(Rx);
-
-                    if (iRx == -1)
-                        return null;
-
-                    if (iRx == len - 1 && X[iRx] == p)
-                        return Y[len - 1];
-
-                    if (iRx == 0)
-                        return Y[0];
-
-                    iRx = lX.IndexOf(Rx) - 1;
-                    Rx = p - X[iRx];
-                    result = a[iRx] + Rx * (b[iRx] + Rx * (c[iRx] + Rx * d[iRx]));
-
-                    return result;
-                }
-                else return null;
-            }
-            else return null;
-        }
-
-        //보간 시킬 데이터(x, y축 배열) 입력
+        // f(x) = 1/(1+x^2)*sin(x)
         public CubicSplineInterpolation(double[] _x, double[] _y)
         {
             int xLength = _x.Length;
-            //배열에 인자 넣기
-            if (xLength == _y.Length && xLength > 1)
+            if (xLength == _y.Length && xLength > 1)    //배열에 인자 넣기
             {
                 x = _x;
                 y = _y;
@@ -313,6 +222,119 @@ namespace Interpolation
             else
                 Console.WriteLine("x,y최소 2개 이상 ");
         }
+
+        // Xk는 X배열의 값, 보간하려는 점의 x좌표
+        // 큐빅 스플라인 공식 = Ak + Bk(X - Xk) + Ck(X - Xk)^2 + Dk(X - Xk)^3
+        public double? Interpolate(double p)
+        {
+            if (baseset)
+            {
+                double? result = 0;
+                int N = len - 1;
+                // x배열의 차이 저장 // X[n]과 X[n+1] 값의 차이
+                // X[n+1] - X[n]
+                double[] h = X.Gap();
+                // D[n] = (Y[n+1] - Y[n] / X[n+1] - X[n])
+                double[] D = Y.Gap().Scale(h, false);
+                // 반복되는 단일 값(3)이 들어있는 시퀀스 생성
+                double[] s = Enumerable.Repeat(3.0, D.Length).ToArray();
+                // 차이값 스케일링
+                // (D[n+1] - D[n]) * 3
+                // ((Y[n+2] - Y[n+1] / X[n+2] - X[n+1]) - (Y[n+1] - Y[n] / X[n+1] - X[n])) * 3
+                double[] dD3 = D.Gap().Scale(s);
+                //Ak
+                double[] a = Y;
+                // 3중 대각선 값들 정리
+                double[,] H = new double[N - 1, N - 1];
+                double[] diagVals = new double[N - 1];
+                for (int i = 1; i < N; i++)
+                {
+                    // diagVals[n] = (X[n+2] - X[n]) * 2
+                    diagVals[i - 1] = 2 * (h[i - 1] + h[i]);
+                }
+                // 대각선에 diagVals[i]
+                //[diagVals[0]                                           ]
+                //[          diagVals[1]                                 ]
+                //[                     diagVals[2]                      ]
+                //[                                diagVals[3]           ]
+                //[                                           diagVals[i]]
+                H = H.Diag(diagVals);
+                
+                // 정사각 행렬일 경우(정방행렬)
+                if (H != null)
+                {
+                    //hk*ck + 2(hK + h(k+1)) * c(k+1) + h(k+1) * c(k+2)
+                    //[diagVals[0]      h[0,1]                                         ]
+                    //[   h[1,0]       diagVals[1]   h[1,2]                              ]
+                    //[                 h[2,1]    diagVals[2]    h[2,3]                  ]
+                    //[                            h[3,2]    diagVals[3]      h[i + 1] ]
+                    //[                                       h[i + 1]    diagVals[i]]
+                    for (int i = 0; i < N - 2; i++)
+                    {
+                        H[i, i + 1] = h[i + 1];//c
+                        H[i + 1, i] = h[i + 1];//a
+                    }
+                    double[] c = new double[N + 2];
+                    c = Enumerable.Repeat(0.0, N + 1).ToArray();
+
+                    // solve tridiagonal matrix
+                    // 토마스 알고리즘을 통해서 d를 구함
+                    // Ck를 구하기 위해서 알고리즘 사용
+                    TridiagonalMatrix matrix = new TridiagonalMatrix();
+                    double[] solution = matrix.MaxtrixSolve(H, dD3);
+                    for (int i = 1; i < N; i++)
+                    {
+                        c[i] = solution[i - 1];
+                    }
+                    //Dk // Bk 를 구하기 위한 공식
+                    double[] b = new double[N];
+                    double[] d = new double[N];
+                    for (int i = 0; i < N; i++)
+                    {
+                        b[i] = D[i] - (h[i] * (c[i + 1] + 2.0 * c[i])) / 3.0;
+                        d[i] = (c[i + 1] - c[i]) / (3.0 * h[i]);
+                    }
+                    double Rx;
+                    try
+                    {
+                        //p값이 x축 좌표계의 범위를 넘어가면 null
+                        // m >= p 조건을 만족하는 첫 요소 리턴
+                        // p 근처의 값을 리턴
+                        Rx = X.First(m => m >= p);
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                    //보간 하고자 하는 지점과 가장 가까운 x축 좌표를 찾는다.
+                    // iRx >> X배열에서 Rx와 일치하는 값의 index
+                    int iRx = lX.IndexOf(Rx);
+                    // 해당하는 값이 없으면 -1
+                    if (iRx == -1)
+                        return null;
+                    // 해당하는 값이 마지막 값이거나, p값과 같으면 마지막값 리턴
+                    if (iRx == len - 1 && X[iRx] == p)
+                        return Y[len - 1];
+                    // 처음 값이면 Y[0] 리턴
+                    if (iRx == 0)
+                        return Y[0];
+                    iRx = lX.IndexOf(Rx) - 1;
+                    // p >> 알고 싶은 값의 x축 좌표
+                    // X[iRx] >> p좌표 근처의 x축 데이터 값
+                    Rx = p - X[iRx];
+                    // 큐빅 스플라인 공식을 풀어쓴 코드
+                    // result = a[iRx] + Rx * b[iRx] + Rx * Rx * c[iRx] + Rx * Rx * Rx * d[iRx];
+                    result = a[iRx] + Rx * (b[iRx] + Rx * (c[iRx] + Rx * d[iRx]));
+                    return result;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+                return null;
+        }
         public double[] X
         {
             get
@@ -320,7 +342,6 @@ namespace Interpolation
                 return x;
             }
         }
-
         public double[] Y
         {
             get
